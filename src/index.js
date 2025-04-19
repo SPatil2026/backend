55a671fba5e5cbc2e03c518460ec0b6642599058
Samarth Patil <samarthpatil2022.comp@mmcoe.edu.in>
2025-04-19 10:31:31 +0530

    util files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 require('dotenv').config({path: './.env'})
+const app = require('./app.js');
 const connectDB = require('./db/index.js')
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.error('MongoDB connection failed !!! :', error);
+});
 
 
 
