a0e40649b71fb0f20ac046b3455eb5161bd20990
Samarth Patil <samarthpatil2022.comp@mmcoe.edu.in>
2025-04-19 11:17:05 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile: {
+            type: String,   // cloudinary URL
+            required: true
+        },
+        thumbnail: {
+            type: String,   // cloudinary URL
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        published: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        }
+    }, 
+    {
+        timestamps: true
+    }
+);
+
+videoSchema.plugin(mongoosePaginate);
+
+export const Video = mongoose.model('Video', videoSchema);
